7617f41fb761419c4f393990585a454b63e98de2
TheZallen <ZacharyAllen2016@u.northwestern.edu>
2014-03-19 19:10:45 -0500

    added copy

--- js/application.js
+++ js/application.js
@@ -25,6 +25,21 @@ String.prototype.repeat = function(num) {
   };
 
   $(function() {
+
+    $(".scroll").click(function (event) {
+        event.preventDefault();
+        //calculate destination place
+        var dest = 0;
+        if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
+            dest = $(document).height() - $(window).height();
+        } else {
+            dest = $(this.hash).offset().top;
+        }
+        //go to destination
+        $('html,body').animate({
+            scrollTop: dest
+        }, 1000, 'easeOutCubic');
+    });
   
     // Todo list
     $(".todo").on('click', 'li', function() {
